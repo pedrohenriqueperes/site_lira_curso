9bc71ba2b0961886ce5fc04795d71da6601bd499
Pedro Peres <php741@protonmail.com>
2024-07-04 20:47:39 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__)
+
+lista_usuarios = ['Lira', 'John', 'Pereira', 'Sogra']
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+if __name__ == '__main__':
+    app.run(debug=True)
