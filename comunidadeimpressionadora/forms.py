45b9db641e6270c4f86404e0b73f754fe4b70dbe
Mac <mac@MacBook-Air-de-Mac.local>
2024-07-11 22:20:46 -0300

    aula39

--- comunidadeimpressionadora/forms.py
+++ comunidadeimpressionadora/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from comunidadeimpressionadora.models import Usuario
+from flask_login import current_user
 
 
 class FormCriarConta(FlaskForm):
@@ -27,4 +29,11 @@ class FormLogin(FlaskForm):
 class FormEditarPerfil(FlaskForm):
     username = StringField('Nome de usuário', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
+    foto_perfil = FileField('Atualizar Foto de Perfil', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
     botao_submit_editarperfil = SubmitField('Confirmar Edição')
+    
+    def validate_email(self, email):
+        if current_user.email != email.data: 
+            usuario = Usuario.query.filter_by(email=email.data).first()
+            if usuario:
+                raise ValidationError('Já existe um usuario com esse e-mail. Cadastre outro e-mail.')
